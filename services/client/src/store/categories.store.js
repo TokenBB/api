73709cad54351d48700303c4f7696fa673f362ea
Jonathan Dupré <hello@jonathandupre.com>
2018-09-22 13:00:16 -0400

    settings, add, remove categories

--- services/client/src/store/categories.store.js
+++ services/client/src/store/categories.store.js
@@ -11,6 +11,14 @@ export default {
     setFetching (state, fetching) {
       state.fetching = fetching
     },
+    add (state, category) {
+      state.categoryList.push(category)
+    },
+    remove (state, category) {
+      var index = state.categoryList.findIndex(c => c.id === category.id)
+
+      state.categoryList.splice(index, 1)
+    },
     updateCategoryList (state, categories) {
       state.categoryList = categories
       categories.forEach(category => {
@@ -19,6 +27,34 @@ export default {
     }
   },
   actions: {
+    add ({ commit }, name) {
+      commit('setFetching', true)
+
+      apiService.addCategory(name)
+        .then(category => {
+          commit('add', category)
+          commit('setFetching', false)
+        })
+        .catch(err => {
+          commit('setFetching', false)
+
+          console.error(err)
+        })
+    },
+    remove ({ commit }, category) {
+      commit('setFetching', true)
+
+      apiService.removeCategory(category.name)
+        .then(categories => {
+          commit('remove', category)
+          commit('setFetching', false)
+        })
+        .catch(err => {
+          commit('setFetching', false)
+
+          console.error(err)
+        })
+    },
     fetchAll ({ commit }) {
       commit('setFetching', true)
 
--- services/client/src/views/Settings.vue
+++ services/client/src/views/Settings.vue
@@ -1,13 +1,72 @@
 <template>
   <div class="container">
+    <h2 class="title is-4">Settings</h2>
+
+    <h3 class="title is-5">Categories</h3>
+    <ul>
+        <li v-for="category in categoryList">
+          <span>{{ category.name }}</span> 
+          <a class="button is-small"
+            :class="{ 'is-loading': fetching }" 
+            @click="remove(category)">
+            <b-icon
+              icon="close-circle"
+              size="is-small">
+            </b-icon>
+          </a>
+        </li>
+    </ul>
+
+    <form @submit.prevent="add">
+      <b-field label="Category Name">
+        <b-input v-model="name"
+          :disabled="fetching">  
+        </b-input>
+      </b-field>
+
+      <button role="submit"
+        class="button" 
+        :class="{ 'is-loading': fetching }"
+        :disabled="fetching">
+        Add
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'settings',
   data () {
     return {
+      name: ''
+    }
+  },
+  computed: {
+    ...mapState('categories', [
+      'categoryList',
+      'fetching'
+    ])
+  },
+  methods: {
+    add () {
+      this.$store.dispatch('categories/add', this.name)
+        .then(() => {
+          this.name = ''
+        })
+        .catch(err => {
+          console.error(err)
+          this.fetching = false
+        })
+    },
+    remove (category) {
+      this.$store.dispatch('categories/remove', category)
+        .catch(err => {
+          console.error(err)
+          this.fetching = false
+        })
     }
   }
 }
